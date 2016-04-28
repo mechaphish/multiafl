aa8b6f765ed3e24aeb5748812446104133edadbc
Jacopo Corbetta <jacopo.corbetta@gmail.com>
2016-04-27 20:31:08 -0700

    Check both for-testing and for-release, if both exist

--- fakeforksrv/check-cs.sh
+++ fakeforksrv/check-cs.sh
@@ -4,6 +4,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PATH="$PATH:../../../darpa/interact/cb-testing/"
 PORT=$(( 2000 + ($RANDOM % 3000) ))
 
+POLLS_DIR_2=
+
 if (( $# > 1 )); then
     POLLS_DIR="$1"
     shift
@@ -14,7 +16,12 @@ elif (( $# == 1 )); then
     [ -d "$SAMPLE_DIR" ] || SAMPLE_DIR="../../../samples/cqe-challenges/$SAMPLE_NAME"
     [ -d "$SAMPLE_DIR" ] || { echo "Cannot find sample dir! Run with full paths. Looked in ../../samples/{examples,cqe-challenges}/$SAMPLE_NAME"; exit 2; }
     POLLS_DIR="$SAMPLE_DIR/poller/for-testing"
-    [ -d "$POLLS_DIR" ] || POLLS_DIR="$SAMPLE_DIR/poller/for-release/"
+    if [ -d "$POLLS_DIR" ]; then
+        POLLS_DIR_2="$SAMPLE_DIR/poller/for-release/"
+        [ -d "$POLLS_DIR_2" ] || POLLS_DIR_2=
+    else
+        POLLS_DIR="$SAMPLE_DIR/poller/for-release/"
+    fi
     [ -d "$POLLS_DIR" ] || { echo "Found sample dir but not its poller/for-{testing,release}/? (sample dir: $SAMPLE_DIR)"; exit 1; }
     CBS="$SAMPLE_DIR/bin/${SAMPLE_NAME}_?"
     [ -f "$SAMPLE_DIR/bin/${SAMPLE_NAME}_1" ] || { echo "Warning: single-CB! Trying $SAMPLE_DIR/bin/$SAMPLE_NAME"; CBS="$SAMPLE_DIR/bin/${SAMPLE_NAME}"; }
@@ -29,6 +36,7 @@ ls $CBS >/dev/null || { echo "No CBs $CBS"; exit 3; }
 
 RET=1
 cb-replay --host 127.0.0.1 --port $PORT "$POLLS_DIR"/*.xml | egrep 'failed: [1-9]' || RET=0
+(( $RET == 1 )) || ! [ -n "$POLLS_DIR_2" ] || cb-replay --host 127.0.0.1 --port $PORT "$POLLS_DIR_2"/*.xml | egrep 'failed: [1-9]' || RET=0
 
 kill %
 exit $RET
