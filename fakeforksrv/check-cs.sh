8b6de597ace1efbdee44077de6f5acdd646e5303
Jacopo Corbetta <jacopo.corbetta@gmail.com>
2016-04-27 20:11:22 -0700

    make check the challenge sets in parallel

--- fakeforksrv/check-cs.sh
+++ fakeforksrv/check-cs.sh
@@ -12,11 +12,12 @@ elif (( $# == 1 )); then
     SAMPLE_NAME="$1"
     SAMPLE_DIR="../../../samples/examples/$SAMPLE_NAME"
     [ -d "$SAMPLE_DIR" ] || SAMPLE_DIR="../../../samples/cqe-challenges/$SAMPLE_NAME"
-    [ -d "$SAMPLE_DIR" ] || { echo "Cannot find sample dir! Run with full paths. Looked in ../../samples/{examples,cqe-challenges}/$SAMPLE_DIR"; exit 2; }
+    [ -d "$SAMPLE_DIR" ] || { echo "Cannot find sample dir! Run with full paths. Looked in ../../samples/{examples,cqe-challenges}/$SAMPLE_NAME"; exit 2; }
     POLLS_DIR="$SAMPLE_DIR/poller/for-testing"
     [ -d "$POLLS_DIR" ] || POLLS_DIR="$SAMPLE_DIR/poller/for-release/"
     [ -d "$POLLS_DIR" ] || { echo "Found sample dir but not its poller/for-{testing,release}/? (sample dir: $SAMPLE_DIR)"; exit 1; }
     CBS="$SAMPLE_DIR/bin/${SAMPLE_NAME}_?"
+    [ -f "$SAMPLE_DIR/bin/${SAMPLE_NAME}_1" ] || { echo "Warning: single-CB! Trying $SAMPLE_DIR/bin/$SAMPLE_NAME"; CBS="$SAMPLE_DIR/bin/${SAMPLE_NAME}"; }
 else
     echo -e "Usage: $0 POLLS_DIR CB_0 [CB_1] [...]\n       SAMPLE_NAME"
     exit 1
